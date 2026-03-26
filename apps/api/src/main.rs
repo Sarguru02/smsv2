abbb3d1a697e48a4ea77f861207e617bf9f1acd4
Sarguru02 <psgnhealth@gmail.com>
2026-03-26 19:42:53 +0530

    init - hello world server with axum closes #17

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(hello_world));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, World!"
 }
