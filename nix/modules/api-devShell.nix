81f776ef77198f79c20b5725b0a7e715a3526d20
Sarguru02 <psgnhealth@gmail.com>
2026-03-26 18:54:26 +0530

    feat(monorepo): migrate to monorepo
    1. moved web app to apps/web
    2. created a new rust project under apps/api
    3. refactored nix files
    4. use rust-flake for rust project

---
+++ nix/modules/api-devShell.nix
@@ -0,0 +1,23 @@
+{ ... }:
+{
+  perSystem = { self', pkgs, ... }: {
+    devShells.api = pkgs.mkShell {
+      name = "Rust api shell";
+      inputsFrom = [
+        self'.devShells.rust
+        self'.devShells.common
+      ];
+
+      shellHook = ''
+        echo "HELLO FROM Rust DEVsheLL"
+      '';
+      packages = with pkgs; [
+        bacon
+        deadnix
+        diesel-cli
+        config.process-compose.cargo-doc-live.outputs.package
+        sqlite
+      ];
+    };
+  };
+}
